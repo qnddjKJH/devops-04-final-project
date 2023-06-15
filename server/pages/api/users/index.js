1d024199fd10f3d3d730a43696131c0dedcc67ce
ibocok0 <ibocok0@gmail.com>
2023-06-15 23:45:45 +0900

    feat: `/api/users` 경로 GET POST DELETE 추가

--- server/pages/api/users/index.js
+++ server/pages/api/users/index.js
@@ -1,56 +1,71 @@
-import { connectDb, queries } from '../../../utils/database';
-import { verifyToken } from '../../../utils/jwt';
+import { users } from '../../../utils/database';
+// import { verifyToken } from '../../../utils/jwt';
 
-const { getSecrets } = require('../../../utils/secret');
-const secretName = 'finaldb';
+// const { getSecrets } = require('../../../utils/secret');
+// const secretName = 'finaldb';
 
 export default async function handler(req, res) {
-  const conn = await connectDb();
-  const secrets = await getSecrets(secretName);
+  // const conn = await connectDb();
+  // const secrets = await getSecrets(secretName);
 
-  const token = req.headers.authorization?.split(' ')[1];
-  const jwt_secrets = secrets.JWT_SECRET;
-  const decoded = verifyToken(token, jwt_secrets);
+  // const token = req.headers.authorization?.split(' ')[1];
+  // const jwt_secrets = secrets.JWT_SECRET;
+  // const decoded = verifyToken(token, jwt_secrets);
 
-  if (decoded) {
-    if (req.method === 'GET') {
-      try {
-        const [result] = await conn.query(queries.getUser());
-        await conn.end();
-        res.status(200).json(result[0]);
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: '서버 오류' });
-      }
+  // if (decoded) {
+  //   if (req.method === 'GET') {
+  //     try {
+  //       const [result] = await conn.query(queries.getUser());
+  //       await conn.end();
+  //       res.status(200).json(result[0]);
+  //     } catch (error) {
+  //       console.error(error);
+  //       res.status(500).json({ message: '서버 오류' });
+  //     }
+  //   }
+  // } else {
+  //   res.status(401).json({ message: 'Unauthorized' });
+  // }
+
+  if (req.method === 'GET') {
+    res.status(200).json(users);
+  } else if (req.method === 'POST') {
+    // 2023.06.15 [ibocok0] req.body 검증
+    if (req.body.id === undefined) {
+      res.status(400).json('id');
     }
 
-    if (req.method === 'POST') {
-      const conn = await connectDb();
-      const users = [];
+    const userId = req.body.id;
+    const today = Date(Date.now()).toString();
+    const user = {
+      id: userId,
+      username: '김예성',
+      email: 'ibocok0@gmail.com',
+      role: 'streamer',
+      cash: 20000,
+      created_at: today,
+      modified_at: today,
+    };
+    users.push(user);
 
-      try {
-        const [ result ] = await conn.query(queries.getUser());
-        console.log(result);
-        users.push(req.body);
+    res.status(200).json(user);
+  } else if (req.method === 'PUT') {
+    res.status(200).json('PUT user');
+  } else if (req.method === 'PATCH') {
+    res.status(200).json('PATCH user');
+  } else if (req.method === 'DELETE') {
+    if (req.body.id === undefined) {
+      res.status(400).json('id');
+    }
 
-        JSON.stringify({
-          "id": "wngud9646",
-          "password": "lee1234",
-          "username": "이주형",
-          "email": "wngud9646@gmail.com",
-          "role": "streamer",
-          "cash": 20000,
-          "created_at": "23/06/14-09:53:52",
-          "modified_at": "23/06/14-09:53:52"
-        });
-
-        await conn.end();
-        res.status(200).json(users);
-      } catch (err) {
-        res.status(500).json({ error: 'post작성 오류' });
+    let deletedUser = null;
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].id === req.body.id) {
+        deletedUser = users[i];
+        delete users[i];
       }
     }
-  } else {
-    res.status(401).json({ message: 'Unauthorized' });
+    
+    res.status(200).json(deletedUser);
   }
 }
--- server/utils/database.js
+++ server/utils/database.js
@@ -49,6 +49,27 @@ const verify = (user_id, password) => `
   WHERE user_id = "${user_id}" AND password = "${password}"
 `;
 
+const users = [
+  {
+    id: 39590,
+    username: '김예성',
+    email: 'ibocok0@gmail.com',
+    role: 'student',
+    cash: 0,
+    created_at: 'Thu Jun 15 2023 23:35:11 GMT+0900 (대한민국 표준시)',
+    modified_at: 'Thu Jun 15 2023 23:35:11 GMT+0900 (대한민국 표준시)',
+  },
+  {
+    id: 48943,
+    username: '김예건',
+    email: 'wlfka1020@gmail.com',
+    role: 'developer',
+    cash: 9999999999,
+    created_at: 'Thu Oct 11 1994 23:35:23 GMT+0900 (대한민국 표준시)',
+    modified_at: 'Thu Jun 15 2023 23:35:23 GMT+0900 (대한민국 표준시)',
+  },
+];
+
 module.exports = {
   connectDb,
   queries: {
@@ -56,4 +77,5 @@ module.exports = {
     getMission,
     verify,
   },
+  users,
 };
