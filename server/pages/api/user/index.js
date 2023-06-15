5746bce5be6a9619c5722dd9b5f3ea0c3b373e5c
ibocok0 <ibocok0@gmail.com>
2023-06-15 22:35:17 +0900

    feat: eslint와 prettier 추가

--- server/pages/api/user/index.js
+++ server/pages/api/user/index.js
@@ -10,30 +10,27 @@ export default async function handler(req, res) {
 
   const token = req.headers.authorization?.split(' ')[1];
   const jwt_secrets = secrets.JWT_SECRET;
-  const decoded = verifyToken(token, jwt_secrets)
-  
-  if(decoded){
+  const decoded = verifyToken(token, jwt_secrets);
+
+  if (decoded) {
     if (req.method === 'GET') {
-      try{
+      try {
         const [result] = await conn.query(queries.getUser());
         await conn.end();
-
         res.status(200).json(result[0]);
-      
-      
       } catch (error) {
-        console.error(error)
-        res.status(500).json({ message: "서버 오류" })
+        console.error(error);
+        res.status(500).json({ message: '서버 오류' });
       }
     }
 
-  
-    if (req.method === 'POST'){
-      users.push(req.body)
+    if (req.method === 'POST') {
+      users.push(req.body);
 
       res.status(200).json(users);
     }
- } else {
-      res.status(401).json({ message: 'Unauthorized' });
- }
+  } else {
+    res.status(401).json({ message: 'Unauthorized' });
+  }
 }
+
--- server/utils/database.js
+++ server/utils/database.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql2/promise');
-require('dotenv').config()
+require('dotenv').config();
 const { getSecrets } = require('./secret');
 
 const secretName = 'finaldb';
@@ -20,36 +20,40 @@ const connectDb = async () => {
     const password = secrets.PASSWORD;
     const database = secrets.DATABASE;
 
-    const conn = await mysql.createConnection({ host, user, password, database })
-    console.log("데이터베이스 연결")
+    const conn = await mysql.createConnection({
+      host,
+      user,
+      password,
+      database,
+    });
+    console.log('데이터베이스 연결');
     return conn;
+  } catch (e) {
+    console.log(e);
+    throw new Error('데이터베이스 연결 오류');
   }
-  catch(e) {
-    console.log(e)
-    throw new Error("데이터베이스 연결 오류");
-  }
-}
+};
 
 const getUser = () => `
   SELECT *
   FROM user
-`
+`;
 
 const getMission = () => `
   SELECT *
   FROM mission
-`
+`;
 const verify = (user_id, password) => `
   SELECT * 
   FROM user 
   WHERE user_id = "${user_id}" AND password = "${password}"
-`
+`;
 
 module.exports = {
   connectDb,
   queries: {
     getUser,
     getMission,
-    verify
-  }
-}
+    verify,
+  },
+};
