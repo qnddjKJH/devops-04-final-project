37b8820d2c09d69982947f9d1794fc0d2c405d4e
qnddjKJH <qnddjkjh.dev@gmail.com>
2023-06-16 16:48:12 +0900

    hotfix: Resolve merge conflict

--- terraform/modules/securitygroup/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "prv_sg" {
-  value = aws_security_group.mission_link_prvsg.id
-}
-
-output "dynamo-prv-sg" {
-  value = aws_security_group.mission_link_dynamo_prvsg.id
-}
--- terraform/modules/securitygroup/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "sec_vpc_id" {
-  description = "alue of the Name tag for the private_sg"
-  type        = string
-  default     = "terraform21"
-}
