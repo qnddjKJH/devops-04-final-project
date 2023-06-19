36fc29dc2ee319b7ca1a6043d89e43c2d4ad86b9
ibocok0 <ibocok0@gmail.com>
2023-06-20 02:41:51 +0900

    refacor: users 제거

--- server/utils/database.js
+++ server/utils/database.js
@@ -71,27 +71,6 @@ const deactivateMission = (id) => `
   where id = ${id}
 `
 
-const users = [
-  {
-    id: 39590,
-    username: '김예성',
-    email: 'ibocok0@gmail.com',
-    role: 'student',
-    cash: 0,
-    created_at: 'Thu Jun 15 2023 23:35:11 GMT+0900 (대한민국 표준시)',
-    modified_at: 'Thu Jun 15 2023 23:35:11 GMT+0900 (대한민국 표준시)',
-  },
-  {
-    id: 48943,
-    username: '김예건',
-    email: 'wlfka1020@gmail.com',
-    role: 'developer',
-    cash: 9999999999,
-    created_at: 'Thu Oct 11 1994 23:35:23 GMT+0900 (대한민국 표준시)',
-    modified_at: 'Thu Jun 15 2023 23:35:23 GMT+0900 (대한민국 표준시)',
-  },
-];
-
 module.exports = {
   connectDb,
   queries: {
@@ -103,7 +82,6 @@ module.exports = {
     putMission,
     getPostedMission,
     deactivateMission
-  },
-  users,
+  }
 };
 
