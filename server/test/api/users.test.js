d23f6749d1813ef4976ece178be906e58fe98df2
Yegun Kim <ibocok0@gmail.com>
2023-06-21 13:27:56 +0900

    Update users.test.js

--- server/test/api/users.test.js
+++ server/test/api/users.test.js
@@ -1,15 +1,6 @@
 // 2023.06.17 [@ibocok0] https://jestjs.io/docs/getting-started
-import { Container } from 'inversify';
-import { TYPES, container } from '../../utils/container';
 import { createMocks } from 'node-mocks-http';
-
-import Repository from '../../utils/Repository';
-import MemoryDataSource from './memoryDataSource';
-import SecretsManager from '../../utils/SecretsManager';
-import MemorySecrets from './MemorySecrets';
-
 import handleUsers from '../../pages/api/users';
-
 
 describe('/api/users', () => {
   test('when get users', async () => {
