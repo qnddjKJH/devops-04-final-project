64d268ca861a3051da08fd2fa421a12bb8c0fa2f
SangHyupL <pk625890@gmail.com>
2023-06-22 12:02:29 +0900

    modi: dynamodb 연결, post 구현 완료

--- server/pages/api/missions/[missionId]/bet.js
+++ server/pages/api/missions/[missionId]/bet.js
@@ -1,9 +1,16 @@
 import { verifyToken } from '../../../../utils/jwt';
 import { createBetParams } from '../../../../utils/params';
 import { sendEvent } from '../../../../utils/eventBridgeClient';
+import { connectDb, queries } from '../../../../utils/database';
+import AWS from 'aws-sdk';
 
 const { getSecrets } = require('../../../../utils/secret');
 const secretName = 'finaldb';
+
+const docClient = new AWS.DynamoDB.DocumentClient({
+  region: 'ap-northeast-2',
+  endpoint: "http://dynamodb.ap-northeast-2.amazonaws.com"
+});
 
 export default async function handler(req, res) {
   const secrets = await getSecrets(secretName);
@@ -23,9 +30,37 @@ export default async function handler(req, res) {
 
       const result = sendEvent(event)
 
-      res.status(200).json({ message: '베팅 성공!' });
+      const missionId = parseInt(req.query.missionId);
+
+      const conn = await connectDb();
+      const [resultdynamo] = await conn.query(queries.getMissionById(missionId));
+      await conn.end();
+
+      if(resultdynamo.length !== 0) {
+        var params = {
+          TableName: "mission_link_dynamodb_table",
+          Item: {
+            "id": req.body.betid,
+            "user_id": req.body.user_id,
+            "missionid": missionId,
+            "action": req.body.action,
+            "amount": req.body.amount,
+            "transactionId": req.body.transactionId,
+          }
+        }
+      }
+    console.log(params);
+
+    docClient.put(params).promise()
+      .then(data => {
+        res.status(200).json({message: '베팅 성공!'});
+      })
+      .catch(err => {
+        res.status(400).json({message: 'mission is undifiend'});
+      });
     }
   } else {
     res.status(401).json({ message: 'Unauthorized' });
   }
+
 }
