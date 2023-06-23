bf12a03f643dce8c3af808f578daf218610a209a
SangHyupL <pk625890@gmail.com>
2023-06-23 16:20:51 +0900

    modi users 더미데이터, users 삭제

--- server/utils/database.js
+++ server/utils/database.js
@@ -80,27 +80,6 @@ const increasebet = (missionid, increaseamount) => `
   UPDATE missions SET mission_reward = mission_reward + ${increaseamount} WHERE id = ${missionid};
 `
 
-const users = [
-  {
-    id: 39590,
-    username: '김예성',
-    email: 'ibocok0@gmail.com',
-    role: 'student',
-    cash: 0,
-    created_at: 'Thu Jun 15 2023 23:35:11 GMT+0900 (대한민국 표준시)',
-    modified_at: 'Thu Jun 15 2023 23:35:11 GMT+0900 (대한민국 표준시)',
-  },
-  {
-    id: 48943,
-    username: '김예건',
-    email: 'wlfka1020@gmail.com',
-    role: 'developer',
-    cash: 9999999999,
-    created_at: 'Thu Oct 11 1994 23:35:23 GMT+0900 (대한민국 표준시)',
-    modified_at: 'Thu Jun 15 2023 23:35:23 GMT+0900 (대한민국 표준시)',
-  },
-];
-
 module.exports = {
   connectDb,
   queries: {
@@ -115,6 +94,4 @@ module.exports = {
     getEmail,
     increasebet
   },
-  users,
 };
-
