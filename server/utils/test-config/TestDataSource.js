f0ddf57de98f4c450e3d59a527cd89e1d194fc01
qnddjKJH <qnddjkjh.dev@gmail.com>
2023-06-26 17:58:07 +0900

    modi: env update

--- server/utils/test-config/TestDataSource.js
+++ server/utils/test-config/TestDataSource.js
@@ -3,10 +3,10 @@ import mysql from 'mysql2/promise';
 export default class TestDataSource {
 
     async createConnection() {
-        const host = 'localhost'
-        const database = 'testdb'
-        const user = 'root'
-        const password = 'root'
+        const host = process.env.TEST_MYSQL_HOST
+        const database = process.env.TEST_MYSQL_DATABASE
+        const user = process.env.TEST_MYSQL_ROOT
+        const password = process.env.TEST_MYSQL_ROOT_PASSWORD
 
         try {
             return await mysql.createConnection({ host, user, password, database });
