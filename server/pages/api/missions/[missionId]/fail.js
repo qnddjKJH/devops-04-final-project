8554fbf3572e0fb8699c254dde948a864721fc97
SangHyupL <pk625890@gmail.com>
2023-06-26 11:36:44 +0900

    event 폴더를 missins로 이동, 모듈 삭제

--- server/pages/api/missions/[missionId]/fail.js
+++ server/pages/api/missions/[missionId]/fail.js
@@ -1,50 +1,13 @@
 import AWS from 'aws-sdk';
 import { connectDb, queries } from '../../../../utils/database';
-import { handlerfail } from '../../events/fail.js';
-
-const docClient = new AWS.DynamoDB.DocumentClient({
-    region: 'ap-northeast-2',
-    endpoint: "http://dynamodb.ap-northeast-2.amazonaws.com"
-  });
 
 export default async function handler(req, res){
   const missionId = req.body.mission_id
-
-  if(req.method === 'POST') {
-    handlerfail(req, res)
-  }
-
   if(req.method === 'PUT'){   
-      var params = {
-        TableName: "mission_link_dynamodb_table",
-        FilterExpression: "mission_id = :mission_id",
-        ExpressionAttributeValues: {
-          ":mission_id": missionId
-         }
-      };
-    
-      console.log(params)
-    
-      let result
-      await docClient.scan(params).promise()
-      .then(data => {
-          console.log(data);
-          result = data;
-      })
-      .catch(err => {
-          console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
-      });
+      const items = req.body;
 
+      console.log(items)
 
-      const items = result.Items.map(item => {
-        return {
-          user_id: item.user_id,
-          amount: parseInt(item.amount, 10)
-        };
-      });
-
-      console.log(items);
-      
       for (let i = 0; i < items.length; i++) {
         let user_id = items[i].user_id;
         let amount = items[i].amount;
