2f0f7831f357e772721af54ca2920a8dfc2936c0
qnddjKJH <qnddjkjh.dev@gmail.com>
2023-06-27 13:41:23 +0900

    hotifx: 변수 오타 수정

--- terraform/modules/event/source/bet.js
+++ terraform/modules/event/source/bet.js
@@ -15,7 +15,7 @@ module.exports.handler = async (event) => {
   console.log(data)
 
   try {
-    await axios.put(`http://wngud9646.click/api/missions/${missionId}/bet`,mission)
+    await axios.put(`http://wngud9646.click/api/missions/${missionId}/bet`,data)
   }catch(error) {
     console.error(error.stack);
     throw new Error(`axios fail 'PUT /api/missions/${missionId}/bet'`);
