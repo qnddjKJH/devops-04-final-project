34b7f1fc276464b0e8a663a9a9eae1df7b6368f9
wngud9646 <wngud9646@gmail.com>
2023-06-30 11:08:09 +0900

    Test: 새로운 terraform 파일에서 action test

---
+++ tftest/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c9c942bd7bf113a2"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "example-instance"
+  }
+}
