e0489450a634c752487f1d7064a3f8da9b9e2c04
wngud9646 <wngud9646@gmail.com>
2023-06-30 15:20:56 +0900

    Test: 스크립트 자동화 test

--- terraform/modules/database/secretmanager.tf
+++ terraform/modules/database/secretmanager.tf
@@ -1,6 +1,6 @@
 # Creating a AWS secret for database master account (Masteraccoundb)
 resource "aws_secretsmanager_secret" "db_password" {
-  name = "mission_link_secret.final"
+  name = "mission_link_secret.final-test"
   recovery_window_in_days = 0
 }
 
--- terraform/scripts/upload.sh
+++ terraform/scripts/upload.sh
@@ -2,6 +2,8 @@
 
 public_ip=$(aws ec2 --profile=admin describe-instances --instance-id ${1} --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
 
+ssh-keyscan -H $public_ip >> ~/.ssh/known_hosts
+
 ssh -i ./keypair/mission_link.pem ubuntu@$public_ip "mkdir -p /home/ubuntu/.ssh/mlink"
 
 scp -i ./keypair/mission_link.pem ./keypair/mlink_worker_node.pem ubuntu@$public_ip:/home/ubuntu/.ssh/mlink/
